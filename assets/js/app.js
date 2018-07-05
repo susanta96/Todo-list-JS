a14e1a9ea0d8dc1f4f776b56fe000c618b65108b
Susanta Chakraborty <svchost96@gmail.com>
2018-07-05 13:51:25 +0530

    add textarea to the div

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,31 @@
 //variables
-
+const taskList = document.getElementById('task-list');
 
 
 
 
 //Event Listeners
+eventListeners();
 
+function eventListeners() {
+  //Form Submission
+  document.querySelector('#form').addEventListener('submit', newTask);
+
+}
 
 
 
 
 //Functions
+function newTask(e) {
+  e.preventDefault();
 
+
+  //Catch the Textarea value
+  const task = document.getElementById('addtask').value;
+
+  const li = document.createElement('li');
+  li.textContent = task;
+  taskList.appendChild(li);
+}
 
