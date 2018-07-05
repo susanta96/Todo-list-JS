392d347ec329ce5b94e73aef82ab623684fb1e45
Susanta Chakraborty <svchost96@gmail.com>
2018-07-05 15:39:53 +0530

    Add remove button nicely for each task

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,8 +24,23 @@ function newTask(e) {
   //Catch the Textarea value
   const task = document.getElementById('addtask').value;
 
+  //Create the remove Button
+  const removeBtn = document.createElement('a');
+  removeBtn.classList = 'remove-task';
+  removeBtn.textContent = 'X';
+
+
+  //Create <li>
   const li = document.createElement('li');
   li.textContent = task;
+
+
+  //add this button to each task
+  li.appendChild(removeBtn);
+
+  //add to the list
   taskList.appendChild(li);
+
+
 }
 
